bcafba6b1fbbe729c0ec92f656e8ebc529961051
Divyanshgupta030 <145568562+Divyanshgupta030@users.noreply.github.com>
2025-03-10 23:51:56 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,89 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
 
+const paragraph = `this is a simple paragraph that does not have any punctuation it flows continuously without any stops or breaks making it a bit challenging to read but still understandable if you focus on the context words just keep coming together forming a long stream of thoughts without interruption which can sometimes make things interesting or even confusing depending on how you look at it`;
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [currentChar, setCurrentChar] = useState("");
+  const [currentWord, setCurrentWord] = useState("");
+  const [currentParagraph, setCurrentParagraph] = useState("");
+  const [idx, setIdx] = useState(0);
+  const wordsArray = paragraph.split(" ");
+  const myWordsArray = currentParagraph.split(" ");
+  let isWordCorrect = false;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if(e.target.value == "" && e.target.value.length === 0) {
+      return
+    }
+    if(e.target.value.slice(-1) === " " &&  currentParagraph.slice(-1) === " ") {
+      setCurrentWord("");
+      setCurrentChar("");
+      return
+    }
+    const lastChar = e.target.value.slice(-1);
+    console.log("lastChar", lastChar);
+    setCurrentChar(e.target.value.slice(0, 1));
+    setCurrentWord((prev) => prev + lastChar);
+    if (lastChar === " " && e.target.value.length >0) {
+      isWordCorrect = wordCorrect();
+      setCurrentWord("");
+      setIdx((prev) => prev + 1);
+    }
+    setCurrentParagraph(e.target.value);
+  };
+  const wordCorrect = () => {
+    if (currentWord === wordsArray[idx]) {
+      console.log("corrrect");
+      return true;
+    }
+    console.log(currentWord, wordsArray[idx]);
+    console.log("incorrect");
+    return false;
+  };
+  useEffect(() => {
+    // console.log("currentChar", currentChar);
+    // console.log("currentWord", currentWord);
+    console.log("currentParagraph", currentParagraph);
+    console.log("idx", idx);
+    // console.log("wordsArray", wordsArray);
+    // console.log("myWordsArray", myWordsArray);
+  }, [currentParagraph,idx]);
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Backspace") {
+        console.log("Backspace key pressed");
+        if (currentChar === "" && currentWord === "" && currentParagraph.length>0) {
+          const prevParagraph = currentParagraph.slice(0, -1);
+          console.log("prevParagraph", prevParagraph,currentParagraph);
+          console.log("sliced", currentParagraph.slice(-1));
+          setCurrentWord(currentParagraph.slice(-1));
+          setCurrentParagraph((prev) => prev.slice(0, -1));
+          setIdx((prev) => prev - 1);
+          setCurrentWord(myWordsArray[idx]);
+        }
+      }
+      if (event.key === "Space" || event.key=== " ") {
+        // if(currentChar === "" && currentWord === "") {
+        //   setCurrentParagraph(currentParagraph.slice(0, -1));
+        // }
+        console.log("Space key pressed");
+      }
+    };
+    
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+  return (
+    <div className="w-full h-full  flex flex-col items-center justify-center">
+      <div className="w-1/2 text-neutral-500">{paragraph}</div>
+      <input
+        className="w-1/2"
+        value={currentParagraph}
+        onChange={handleChange}
+      />
     </div>
   );
 }
